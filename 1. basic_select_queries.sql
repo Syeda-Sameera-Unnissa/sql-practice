555eb0644d928b731b726dea5cf118ecc4be5a51
Syeda-Sameera-Unnissa <syedasameeraunnissa@gmail.com>
2026-03-16 08:31:16 +0530

    Remove basic SELECT queries from SQL file

---
+++ 1. basic_select_queries.sql
@@ -0,0 +1,5 @@
+-- ==================================================
+-- File: basic_select_queries.sql
+-- Description: Basic SQL SELECT statements practice
+-- Author: Sameera
+-- =====================================================
