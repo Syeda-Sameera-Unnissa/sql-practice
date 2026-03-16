bbe6fdef66e79926b6b800ce76a25a121667b04b
Syeda-Sameera-Unnissa <syedasameeraunnissa@gmail.com>
2026-03-16 08:31:38 +0530

    Add basic SQL SELECT queries examples
    Enhance basic_select_queries.sql with comments

--- 1. basic_select_queries.sql
+++ 1. basic_select_queries.sql
@@ -3,3 +3,22 @@
 -- Description: Basic SQL SELECT statements practice
 -- Author: Sameera
 -- =====================================================
+-- =====================================================
+-- BASIC SELECT QUERIES
+-- =====================================================
+-- Select all columns from Employees table
+SELECT * FROM Employees;
+
+-- Select specific column
+SELECT last_name
+FROM Employees;
+
+-- Select multiple columns
+SELECT last_name, first_name, id
+FROM Employees;
+
+-- Using column aliases
+SELECT last_name AS "Last Name",
+       first_name AS "First Name",
+       id AS "Employee ID"
+FROM Employees;
