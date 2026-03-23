25df824f0d32867e1bb3ff348e5f2927fea5ca66
Syeda-Sameera-Unnissa <syedasameeraunnissa@gmail.com>
2026-03-23 11:54:29 +0530

    Add JOIN query examples for Employees and Departments
    Added examples of INNER JOIN and LEFT JOIN queries to combine data from Employees and Departments tables.

--- 05_joins_queries.sql
+++ 05_joins_queries.sql
@@ -1,5 +1,21 @@
--- ==================================================
--- File: 05_joins_queries.sql
--- Description: SQL JOIN queries combining data from multiple tables
+-- =====================================================
+-- JOINS
 -- Author: Sameera
--- ==================================================
+-- Description: Combining data from multiple tables
+-- =====================================================
+
+-- INNER JOIN example
+SELECT Employees.first_name,
+       Employees.last_name,
+       Departments.department_name
+FROM Employees
+INNER JOIN Departments
+ON Employees.department_id = Departments.id;
+
+-- LEFT JOIN example
+SELECT Employees.first_name,
+       Employees.last_name,
+       Departments.department_name
+FROM Employees
+LEFT JOIN Departments
+ON Employees.department_id = Departments.id;
