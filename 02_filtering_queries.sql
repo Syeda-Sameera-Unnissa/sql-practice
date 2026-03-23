2240699446dcb0c820393fb3aed5c8e413058a79
Syeda-Sameera-Unnissa <syedasameeraunnissa@gmail.com>
2026-03-23 12:04:29 +0530

    Refactor SQL queries for filtering employees

--- 02_filtering_queries.sql
+++ 02_filtering_queries.sql
@@ -4,69 +4,123 @@
 -- Author: Sameera
 -- ==================================================
 
--- Employees from Marketing department
-SELECT *
+-- Select first and last name for employee with ID = 10
+SELECT first_name, last_name
 FROM Employees
-WHERE department = 'Marketing';
+WHERE id = 10;
 
--- Employees with tshirt size L
-SELECT *
-FROM Employees
-WHERE tshirt_size = 'L';
 
--- Employees with more than 9 vacations
-SELECT *
-FROM Employees
-WHERE vacation_taken > 9;
-
--- Employees with less than 9 vacations
-SELECT *
-FROM Employees
-WHERE vacation_taken < 9;
-
--- Employees not equal to 9 vacations
-SELECT *
-FROM Employees
-WHERE vacation_taken <> 9;
-
--- Pattern matching: last name starts with T
-SELECT *
-FROM Employees
-WHERE last_name LIKE 'T%';
-
--- Pattern matching: letter t anywhere
-SELECT *
-FROM Employees
-WHERE last_name LIKE '%t%';
-
--- Pattern matching: second letter a
-SELECT *
-FROM Employees
-WHERE last_name LIKE '_a%';
-
--- Multiple conditions
-SELECT *
-FROM Employees
-WHERE department = 'Marketing'
-AND tshirt_size = 'L';
-
--- OR condition
-SELECT *
-FROM Employees
-WHERE department = 'Sales'
-OR department = 'Marketing';
-
--- Exclude specific name
-SELECT *
-FROM Employees
-WHERE first_name <> 'Riki';
-
--- Limit results
+-- Limit results to first 10 rows
 SELECT *
 FROM Employees
 LIMIT 10;
 
--- Limit with offset
+
+-- Limit results with offset
 SELECT *
 FROM Employees
+WHERE tshirt_size = 'L'
 LIMIT 1 OFFSET 10;
+
+-- Filter employees belonging to Service department
+SELECT *
+FROM Employees
+WHERE department = 'Service';
+
+
+-- Filter employees from Marketing department
+SELECT *
+FROM Employees
+WHERE department = 'Marketing';
+
+
+-- Case sensitive comparison
+SELECT *
+FROM Employees
+WHERE department = 'Sales';
+
+
+-- Case insensitive search using LIKE
+SELECT *
+FROM Employees
+WHERE department LIKE 'sales';
+
+
+-- Filter employees with tshirt size L
+SELECT last_name, first_name, tshirt_size
+FROM Employees
+WHERE tshirt_size = 'L';
+
+
+-- Employees who took exactly 9 vacations
+SELECT *
+FROM Employees
+WHERE vacation_taken = 9;
+
+
+-- Employees who took more than 9 vacations
+SELECT *
+FROM Employees
+WHERE vacation_taken > 9;
+
+
+-- Employees who took 9 or more vacations
+SELECT *
+FROM Employees
+WHERE vacation_taken >= 9;
+
+
+-- Employees who took less than 9 vacations
+SELECT *
+FROM Employees
+WHERE vacation_taken < 9;
+
+
+-- Employees who did not take exactly 9 vacations
+SELECT *
+FROM Employees
+WHERE vacation_taken <> 9;
+
+
+-- Pattern matching: last name starting with T
+SELECT *
+FROM Employees
+WHERE last_name LIKE 'T%';
+
+
+-- Pattern matching: last name containing letter T
+SELECT *
+FROM Employees
+WHERE last_name LIKE '%t%';
+
+
+-- Pattern matching: second letter is 'a'
+SELECT *
+FROM Employees
+WHERE last_name LIKE '_a%';
+
+
+-- Employees not in Sales department
+SELECT first_name, last_name, department
+FROM Employees
+WHERE department IS NOT 'Sales';
+
+
+-- Employees not in Sales and tshirt size not L
+SELECT first_name, last_name, department, tshirt_size
+FROM Employees
+WHERE department IS NOT 'Sales'
+AND tshirt_size IS NOT 'L';
+
+
+-- Employees in Sales OR Marketing department
+SELECT first_name, last_name, department
+FROM Employees
+WHERE department = 'Sales'
+OR department = 'Marketing';
+
+
+-- Exclude employees named Riki
+SELECT first_name, last_name
+FROM Employees
+WHERE first_name <> 'Riki';
