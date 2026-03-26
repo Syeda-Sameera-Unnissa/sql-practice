8de8996f87afc4f7b8df64d72099a384a65e0352
Syeda-Sameera-Unnissa <syedasameeraunnissa@gmail.com>
2026-03-26 07:49:26 +0530

    Create 08_cte_and_views.sql

---
+++ 08_cte_and_views.sql
@@ -0,0 +1,12 @@
+-- ======================================
+-- AUTHOR: Sameera
+-- REPOSITORY: sql-practice
+-- FILE: 08_cte_and_views.sql
+-- TOPIC: Common Table Expressions and Views
+-- DESCRIPTION: Writing reusable queries using CTEs (WITH clause), recursive CTEs, and creating/querying Views
+-- ======================================
+
+-- ======================================
+-- CTE AND VIEWS
+-- ======================================
+
